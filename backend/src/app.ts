902e972d6f224d04c1b1d25fc1ba2a183879c40a
anandasaikiacse <anandasaikiacse@gmail.com>
2026-02-20 16:06:39 +0530

    feat: add Dockerfile for application setup and configure production environment in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
 import { clerkMiddleware } from '@clerk/express'
+
 import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
@@ -15,11 +18,20 @@ app.get("/health", (req, res) => {
     res.json({ status: "ok", message: "Server is running" });
 });
 
-app.use("/api/auth", authRoutes)
-app.use("/api/chats", chatsRoutes)
-app.use("/api/messages", messagesRoutes)
-app.use("/api/users", usersRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
+
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 
 export default app;
