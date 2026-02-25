660dee5da0ae82db8ab2099b3c9aa3ae036b33fd
anandasaikiacse <anandasaikiacse@gmail.com>
2026-02-26 04:34:33 +0530

    feat: integrate Tailwind CSS and DaisyUI for styling, add new pages and components
    - Added Tailwind CSS and DaisyUI dependencies for styling.
    - Created HomePage and ChatPage components for user interaction.
    - Implemented PageLoader component for loading states.
    - Introduced useUserSync hook for user authentication synchronization.
    - Established socket connection management with Zustand for real-time messaging.
    - Removed unused App.css and react.svg files.
    - Updated main entry point to include QueryClientProvider and BrowserRouter.
    - Added new assets for authentication and README screenshot.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
-import { clerkMiddleware } from '@clerk/express'
+import cors from "cors";
+
+import { clerkMiddleware } from "@clerk/express";
 
 import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
@@ -10,12 +12,24 @@ import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
-app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
+const allowedOrigins = [
+  "http://localhost:8081", // expo mobile
+  "http://localhost:5173", // vite web devs
+  process.env.FRONTEND_URL!, // production
+].filter(Boolean);
 
-app.use(clerkMiddleware())
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // allow credentials from client (cookies, authorization headers, etc.)
+  })
+);
+
+app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
+app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-    res.json({ status: "ok", message: "Server is running" });
+  res.json({ status: "ok", message: "Server is running" });
 });
 
 app.use("/api/auth", authRoutes);
@@ -23,6 +37,7 @@ app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// error handlers must come after all the routes and other middlewares so they can catch errors passed with next(err) or thrown inside async handlers.
 app.use(errorHandler);
 
 // serve frontend in production
