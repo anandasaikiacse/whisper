b9a7416ee2cb7d2c571692726e36a37650e23593
anandasaikiacse <anandasaikiacse@gmail.com>
2026-02-26 02:47:21 +0530

    feat: add MessageBubble and SocketConnection components, implement useMessages hook, and update API and socket URLs

--- mobile/lib/axios.ts
+++ mobile/lib/axios.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/react-native";
 import { useAuth } from "@clerk/clerk-expo";
 import { useCallback } from "react";
 
-const API_URL = "https://whisper-app.up.railway.app";
+const API_URL = "https://whisper-app.up.railway.app/api";
 
 // this is the same thing we did with useEffect setup but it's optimized version - it's better!!
 
